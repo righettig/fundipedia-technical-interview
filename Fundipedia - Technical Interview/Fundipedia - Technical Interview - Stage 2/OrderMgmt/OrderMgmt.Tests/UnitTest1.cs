32b0be72043e7563d9ed1317bb8859a14eaf4bfa
Giacomo Righetti <righetti.giacomo@live.com>
2025-01-29 09:42:54 +0000

    Added initial test scenarios

--- Fundipedia - Technical Interview/Fundipedia - Technical Interview - Stage 2/OrderMgmt/OrderMgmt.Tests/UnitTest1.cs
+++ Fundipedia - Technical Interview/Fundipedia - Technical Interview - Stage 2/OrderMgmt/OrderMgmt.Tests/UnitTest1.cs
@@ -2,6 +2,12 @@ namespace OrderMgmt.Tests
 {
     public class UnitTest1
     {
+        // Large repair orders for new customers should be closed
+        // Large rush hire orders should always be closed
+        // Large repair orders always require authorisation
+        // All rush orders for new customers always require authorisation
+        // All other orders should be confirmed
+
         [Fact]
         public void Test1()
         {
