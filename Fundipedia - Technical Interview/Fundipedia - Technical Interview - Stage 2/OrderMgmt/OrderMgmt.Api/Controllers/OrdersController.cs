7bcab0cd81e37e9800c8092cebace94e97d4623d
Giacomo Righetti <righetti.giacomo@live.com>
2025-01-29 09:41:22 +0000

    Cleanup

---
+++ Fundipedia - Technical Interview/Fundipedia - Technical Interview - Stage 2/OrderMgmt/OrderMgmt.Api/Controllers/OrdersController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderMgmt.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrdersController : ControllerBase
+    {
+        public OrdersController()
+        {
+        }
+    }
+}
